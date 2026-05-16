c28177dea0f83cccc314327d6978b56d233bf7ed
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2026-05-16 09:22:13 +0600

    Fixed package.json and render-build.sh for Render deployment

--- render-build.sh
+++ render-build.sh
@@ -1,17 +1,16 @@
+
+
+#!/usr/bin/env bash
+
 # Ai file er all command package.json file a likhte hoi. And akta akta kore command dita hoto. But aksathe all command exicute korar jonno ai file a sobgulo command likhtesi. Pore sudho ai file ke run korlei all command exicute hoia jabe. Aita lagbe jokhon render a deploy korbo tokhon.
 
 set -o errexit
 
+# ১. প্যাকেজ ইনস্টল করা (এটার সাথেই postinstall রান হয়ে prisma generate হয়ে যাবে)
 npm install
+
+# ২. প্রজেক্ট বিল্ড করা
 npm run build
-npx prisma generate
-npx prisma migrate deploy
 
-
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# npm install
-# npx prisma generate --schema=./prisma/schema
-# npm run build
+# ৩. ডাটাবেস মাইগ্রেশন রান করা (প্রোডাকশনের জন্য deploy কমান্ড ব্যবহার করতে হয়)
+npx prisma migrate deploy --schema=./prisma/schema
